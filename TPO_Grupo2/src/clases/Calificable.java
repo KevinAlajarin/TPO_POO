f19e1c177c110306f30a4affe1b892dd79d10bb3
Kevin Alajarin <82348095+KevinAlajarin@users.noreply.github.com>
2024-11-19 21:44:32 -0300

    Delete TPO_Grupo2 directory

--- TPO_Grupo2/src/clases/Calificable.java
+++
@@ -1,5 +0,0 @@
-package clases;
-
-public interface Calificable {
-    void calificar(Curso curso, Docente docente, int calificacionCurso, int calificacionDocente);
-}
--- TPO_Grupo2/src/clases/Validable.java
+++
@@ -1,5 +0,0 @@
-package clases;
-
-public interface Validable {
-    boolean validarDocente(Docente docente);
-}
